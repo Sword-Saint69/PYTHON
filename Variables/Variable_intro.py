8ff5fd8b41f8fd32ebc1e2cb4f83de7ba8e9e90b
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-05-30 22:37:27 +0530

    commit

---
+++ Variables/Variable_intro.py
@@ -0,0 +1,25 @@
+#Variables in Python
+
+#Variable --> Containers that store data
+
+# variable declaration and initialization
+
+name = "Goutham"
+
+age = 22
+
+x= 22
+
+print(id(age))
+print(id(x))
+
+#Valid variable names
+
+name= "Goutham"
+_age=12
+num1ber=123
+
+#Invalid variable name
+@name= "Goutham"
+!name = 12
+namee%uni = 12
