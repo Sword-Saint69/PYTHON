2a31ccb1f900b4510ac2e1205313f67d8c9f8901
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-06-01 22:48:46 +0530

    Day 2

---
+++ DataTypes/Tuple.py
@@ -0,0 +1,27 @@
+#Tuple
+
+tup1=(2,3,12,13,43)
+print(tup1)
+print(type(tup1))
+
+#Using Constructor
+
+T1=tuple([1,32,21,34,21,12])
+print(T1)
+
+T2=tuple("Goutham")
+print(T2)
+
+#When there is only one item in the tuple
+
+T3 = (34,)
+print(type(T3))
+
+fruits = ("Apple","Orange","Mango","Strawberry","Apple")
+print(fruits.count("Apple"))
+
+print(fruits.index("Mango"))
+
+print(len(fruits))
+
+print(fruits[3])
