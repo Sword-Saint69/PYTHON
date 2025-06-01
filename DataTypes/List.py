f2f41491c80245d3dfca4160f791aa2cb4f84bee
GOUTHAM SANKAR J L <gouthamsankar@aol.com>
2025-06-01 19:29:50 +0530

    Day 1

---
+++ DataTypes/List.py
@@ -0,0 +1,55 @@
+#Creatig a list
+
+Students = ["Goutham","Krishna","Rene"]
+print(Students)
+
+print(type(Students))
+
+
+#Using constructor method
+
+list4 = list(Students)
+print(list)
+
+list2 = list("Goutham")
+print(list2)
+
+list3 = list((3,4,"goutham"))
+print(list3)
+
+#Accesing Elements of List
+
+l1 = [1,2,4,5,67,8,3,22]
+
+print(l1[3])
+print(l1[5])
+
+#Inserting Elements in a List
+
+arr = [3,32,2,12,14,55,565,34,21]
+arr.append(11)
+print(arr)
+
+#Inserting multiple elements in a array
+arr = [3, 32, 2, 12, 14, 55, 565, 34, 21]
+arr.insert(2, (23, 23, 121))  # Insert tuple at index 2
+print(arr)
+
+
+l1 = [1,2]
+l2 = ["Goutham","Sankar"]
+l1.extend(l2)
+print(l1)
+
+#Removing Element
+arr1 = [3,5,2,23,23,32,21]
+
+arr1.remove(2)
+print(arr1)
+
+
+arr1.pop()
+print(arr1)
+
+
+#R
